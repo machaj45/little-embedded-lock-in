038eb8c3c5e6256235d2495bb86c09e16ac758ad
Jan Machálek <machaj45@fel.cvut.cz>
2021-02-21 11:42:49 +0100

    Update Help with link

--- Application/HelpWindow.py
+++ Application/HelpWindow.py
@@ -11,26 +11,36 @@ class HelpWindow(QWidget):
     def __init__(self, icon):
         super().__init__()
         layout = QVBoxLayout()
-        self.label = QLabel("This is help to lock-in amplifier that was created by Jan Machálek,\n"
-                            "as part of my Diploma Thesis. There is two mods to select from sin mode and rect mode\n"
-                            "In sin mode you get gain and phase in rect mod you get gain and output voltage.\n"
-                            "How to start?\n"
-                            "1) you have to connect device with right firmware.\n"
-                            "2) then you have to set up generator on channel one.\n"
-                            "3) you have to select number of samples per period\n"
-                            "4) you can select continuous or single measurement\n"
-                            "   or you can use automatic measurement\n"
-                            "   this option opens dialog to specify csv file with\n"
-                            "   frequencies that you want to measure on.\n"
-                            "   after automatic measurements is completed you can\n"
-                            "   use save as button to save data that has been measured.\n"
-                            "\n"
-                            "How to select another lock-in? "
-                            "You can use scan button that will update dropdown at the top\n"
-                            "of application then you can select comport that you want\n"
-                            "and then connect to that port using connect button."
-                            "For more information look at  ")
+        layout.setContentsMargins(0,210,0,110)
+        self.label = QLabel()
+        self.label_link = QLabel()
+
+        self.label.setText("This is help to lock-in amplifier that was created by Jan Machálek,\n"
+                           "as part of my Diploma Thesis. There is two mods to select from sin mode and rect mode\n"
+                           "In sin mode you get gain and phase in rect mod you get gain and output voltage.\n"
+                           "How to start?\n"
+                           "1) you have to connect device with right firmware.\n"
+                           "2) then you have to set up generator on channel one.\n"
+                           "3) you have to select number of samples per period\n"
+                           "4) you can select continuous or single measurement\n"
+                           "   or you can use automatic measurement\n"
+                           "   this option opens dialog to specify csv file with\n"
+                           "   frequencies that you want to measure on.\n"
+                           "   after automatic measurements is completed you can\n"
+                           "   use save as button to save data that has been measured.\n"
+                           "\n"
+                           "How to select another lock-in? "
+                           "You can use scan button that will update dropdown at the top\n"
+                           "of application then you can select comport that you want\n"
+                           "and then connect to that port using connect button.\n\n"
+                           "For more information look at:\n"
+                           )
+        url_link = "<a href=\"https://github.com/machaj45/little-embedded-lock-in/blob/main/Manual/manual.pdf\">Manual PDF</a>"
+        self.label_link.setOpenExternalLinks(True)
+        self.label_link.setText(url_link)
         layout.addWidget(self.label)
+        layout.addWidget(self.label_link)
+
         self.setLayout(layout)
         self.icon = icon
         self.setWindowIcon(self.icon)
